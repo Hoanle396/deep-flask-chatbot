1bb3910af819a3ce58d77e9358cd410791cce040
Lê Hữu Hoàn <hoanle396@gmail.com>
2023-06-13 21:59:36 +0700

    update

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,5 @@
 from app import app
 import app.settings as settings
-from flask_frozen import Freezer
 from controllers.route import registryRouter
 from models.model import ChatBot
 
@@ -8,5 +7,4 @@ model = ChatBot()
 
 if __name__ == "__main__":
     registryRouter(app, model)
-    freezer = Freezer(app)
-    freezer.freeze()
+    app.run(host=settings.BE_HOST, port=settings.BE_PORT, debug=settings.ENV != "PROD")
